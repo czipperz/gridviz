eb7bc2d773d3732dc60e5b44d8a4a7f9497d0379
Chris Gregory <czipperz@gmail.com>
2021-11-28 22:19:46 -0800

    congridviz draw a grid

--- congridviz.cpp
+++ congridviz.cpp
@@ -12,15 +12,36 @@ int main() {
     }
 
     std::random_device rd;
-    std::uniform_int_distribution<int> dist(0, 9);
+    std::uniform_int_distribution<int> parse_dist(0, 1);
+    std::uniform_int_distribution<int> destroy_dist(0, 3);
 
-    netgridviz_context c1 = netgridviz_create_context();
-    netgridviz_context c2 = netgridviz_create_context();
+    netgridviz_context normal = netgridviz_create_context();
+    netgridviz_context destroyed = netgridviz_create_context();
 
-    netgridviz_set_fg(&c1, 0xff, 0, 0);
+    netgridviz_set_fg(&destroyed, 0xff, 0, 0);
 
-    netgridviz_draw_char(&c1, /*x=*/dist(rd), /*y=*/dist(rd), '#');
-    netgridviz_draw_char(&c2, /*x=*/dist(rd), /*y=*/dist(rd), 'X');
+    char grid[10][10];
+
+    netgridviz_start_stroke("Parse");
+    // Visualize a grid as you parse it.
+    for (int y = 0; y < 10; ++y) {
+        for (int x = 0; x < 10; ++x) {
+            grid[y][x] = (parse_dist(rd) ? '#' : '.');
+            netgridviz_draw_char(&normal, x, y, grid[y][x]);
+        }
+    }
+    netgridviz_end_stroke();
+
+    for (int y = 0; y < 10; ++y) {
+        for (int x = 0; x < 10; ++x) {
+            if (grid[y][x] == '#') {
+                if (destroy_dist(rd) == 0) {
+                    grid[y][x] = '.';
+                    netgridviz_draw_char(&destroyed, x, y, grid[y][x]);
+                }
+            }
+        }
+    }
 
     netgridviz_disconnect();
 }
