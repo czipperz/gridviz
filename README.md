86d278463afe9b414dfde5574b5d244af7b4f448
Chris Gregory <czipperz@gmail.com>
2021-11-29 00:23:10 -0800

    Improve readme, reorder congridviz and use mt19937

--- README.md
+++ README.md
@@ -3,15 +3,28 @@
 gridviz is a Grid Visualizer.  It is specifically useful
 for debugging applications that show a 2D matrix of text.
 
-You can integrate gridviz into your application via:
-* Linking the `netgridviz` library.  This library will establish a connection to
-  the `gridviz` server and provides a simple C interface as well as C++ wrappers.
-
 This repository is licensed under GPL3.  If you wish to
 purchase a different license, email czipperz AT gmail DOT com.
 
+## Integration
+
+You can integrate gridviz into your application via the
+`netgridviz` library.  This library will establish a connection
+to the `gridviz` server and provides a simple C interface.
+
+`netgridviz.h` is an all in one header for `netgridviz`.  Just include it in
+your project and `#define NETGRIDVIZ_DEFINE` in one file.  Alternatively, use
+the `netgridviz` library that is automatically built as part of `gridviz`.
+
+## Overview
+
 Features:
-* TODO writeme
+* Draw commands are batched into strokes.
+* Each stroke has a user configured title.
+* Clicking on a stroke in the timeline rewinds the visual state to when the
+  stroke occurred, allowing you to easily undo actions and see what went wrong.
+
+TODO add gifs
 
 ## Building
 
--- congridviz.cpp
+++ congridviz.cpp
@@ -5,37 +5,42 @@
 #include <random>
 
 int main() {
+    // Connect to server.
     int result = netgridviz_connect(NETGRIDVIZ_DEFAULT_PORT);
     if (result != 0) {
         fprintf(stderr, "Failed to connect!\n");
         return 1;
     }
 
+    // Setup state.
+    netgridviz_context normal = netgridviz_create_context();
+    netgridviz_context destroyed = netgridviz_create_context();
+    netgridviz_set_fg(&destroyed, 0xff, 0, 0);
+
+    // Random number generators.
     std::random_device rd;
+    std::mt19937 gen(rd());
     std::uniform_int_distribution<int> parse_dist(0, 1);
     std::uniform_int_distribution<int> destroy_dist(0, 3);
 
-    netgridviz_context normal = netgridviz_create_context();
-    netgridviz_context destroyed = netgridviz_create_context();
+    // Make a demo grid.
+    char grid[10][10] = {};
 
-    netgridviz_set_fg(&destroyed, 0xff, 0, 0);
-
-    char grid[10][10];
-
-    netgridviz_start_stroke("Parse");
     // Visualize a grid as you parse it.
+    netgridviz_start_stroke("Parse");
     for (int y = 0; y < 10; ++y) {
         for (int x = 0; x < 10; ++x) {
-            grid[y][x] = (parse_dist(rd) ? '#' : '.');
+            grid[y][x] = (parse_dist(gen) ? '#' : '.');
             netgridviz_draw_char(&normal, x, y, grid[y][x]);
         }
     }
     netgridviz_end_stroke();
 
+    // Make some random changes.  Note: each draw command is sent as a separate stroke.
     for (int y = 0; y < 10; ++y) {
         for (int x = 0; x < 10; ++x) {
             if (grid[y][x] == '#') {
-                if (destroy_dist(rd) == 0) {
+                if (destroy_dist(gen) == 0) {
                     grid[y][x] = '.';
                     netgridviz_draw_char(&destroyed, x, y, grid[y][x]);
                 }
